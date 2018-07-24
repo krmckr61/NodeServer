df1ee5094b1ab9d02086b1cc8c3ac7216df40572
krmckr61 <kerem.cakirr@gmail.com>
2018-07-24 19:00:00 +0300

    -fix spagetti

--- Socket/Server/SocketListener.js
+++ Socket/Server/SocketListener.js
@@ -90,9 +90,9 @@ SocketListener.prototype.destroyChat = function (userId, siteId, visitId, socket
                         Client.destroyChat(clientId);
                         Trigger.destroyChat(clientId, visitId, message, io);
                         Trigger.clientDisconnect(clientId, client.siteId, io);
-                        // ClientSocketController.reconnectClient(clientId, siteId, socket, io);
                         Visit.autoTakeClients(Server.getAll(), io);
                         Trigger.clientDisconnectChat(visitId, io);
+                        io.sockets.emit('newClient', client);
                     });
                 });
             }
